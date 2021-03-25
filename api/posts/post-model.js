31098eefef345db90d9f02cea98ec7ee5ac8c31e
ngwatso <ngwatson79@yahoo.com>
2021-03-25 14:31:04 -0400

    complete

--- api/posts/post-model.js
+++ api/posts/post-model.js
@@ -1,27 +1,40 @@
+const db = require('../../data/db-config');
+
 module.exports = {
-  get,
-  getById,
-  create,
-  update,
-  remove,
+	get,
+	getById,
+	create,
+	update,
+	remove,
+};
+
+async function get() {
+	console.log(await db('posts').toString());
+	// const sql = await db('posts').toString();
+	// console.log(sql);
+	return db('posts');
+	// return Promise.resolve('get wired');
 }
 
-function get() {
-  return Promise.resolve('get wired')
+function getById(id) {
+	const post = db.first('*').from('posts').where({ id });
+	return post;
+	// ** return Promise.reject('test-rejection)
+	// return Promise.resolve('getById wired');
 }
 
-function getById() {
-  return Promise.resolve('getById wired')
+async function create(newPost) {
+	const post = await db('posts').insert(newPost);
+	return post;
+	// return Promise.resolve('create wired');
 }
 
-function create() {
-  return Promise.resolve('create wired')
+function update(id, newData) {
+	return db('posts').update(newData).where({ id });
+	// return Promise.resolve('update wired');
 }
 
-function update() {
-  return Promise.resolve('update wired')
-}
-
-function remove() {
-  return Promise.resolve('delete wired')
+function remove(id) {
+	return db('posts').del().where({ id });
+	// return Promise.resolve('delete wired');
 }
--- api/posts/post-router.js
+++ api/posts/post-router.js
@@ -1,63 +1,82 @@
-const express = require('express')
-const Post = require('./post-model')
+const express = require('express');
+const Post = require('./post-model');
 
-const router = express.Router()
+const router = express.Router();
 
-function checkId(req, res, next) {
-  next()
+async function checkId(req, res, next) {
+	try {
+		const post = await Post.getById(req.params.id);
+		if (post) {
+			req.post = post;
+			next();
+		} else {
+			const err = new Error('id not found');
+			err.statusCode = 404;
+			next(err);
+		}
+	} catch (err) {
+		err.statusCode = 500;
+		next(err);
+	}
 }
 
 function checkPayload(req, res, next) {
-  next()
+	const newPost = req.body;
+	if (!newPost.title && !newPost.contents) {
+		const err = new Error('body must include title, contents, or both');
+		err.statusCode = 400;
+		next(err);
+	} else {
+		next();
+	}
 }
 
 router.get('/', async (req, res, next) => {
-  try {
-    const data = await Post.get()
-    res.json(data)
-  } catch (err) {
-    next(err)
-  }
-})
+	try {
+		const data = await Post.get();
+		res.json(data);
+	} catch (err) {
+		next(err);
+	}
+});
 
-router.get('/:id', checkId, async (req, res, next) => {
-  try {
-    const data = await Post.getById()
-    res.json(data)
-  } catch (err) {
-    next(err)
-  }
-})
+router.get('/:id', checkId, (req, res) => {
+	res.status(200).json(req.post);
+});
 
 router.post('/', checkPayload, async (req, res, next) => {
-  try {
-    const data = await Post.create()
-    res.json(data)
-  } catch (err) {
-    next(err)
-  }
-})
+	try {
+		const data = await Post.create(req.body);
+		res.status(201).json(data);
+	} catch (err) {
+		next(err);
+	}
+});
 
-router.put('/:id', checkPayload, checkId, async (req, res,next ) => {
-  try {
-    const data = await Post.update()
-    res.json(data)
-  } catch (err) {
-    next(err)
-  }
-})
+router.put('/:id', checkPayload, checkId, async (req, res, next) => {
+	try {
+		const data = await Post.update(req.params.id, req.body);
+		res.json(data);
+	} catch (err) {
+		next(err);
+	}
+});
 
 router.delete('/:id', checkId, async (req, res, next) => {
-  try {
-    const data = await Post.remove()
-    res.json(data)
-  } catch (err) {
-    next(err)
-  }
-})
+	try {
+		const data = await Post.remove(req.params.id);
+		res.json(data);
+	} catch (err) {
+		next(err);
+	}
+});
 
 router.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message, stack: err.stack })
-})
+	err.statusCode = err.statusCode || 500;
+	res.status(err.statusCode).json({
+		message: err.message,
+		stack: err.stack,
+	});
+});
 
-module.exports = router
+module.exports = router;
